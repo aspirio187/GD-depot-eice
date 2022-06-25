1dff9b91994bbcca2e048b14236bec47fe4f28f6
Soultan Hatsijev <soultan_98@hotmail.com>
2022-06-25 15:17:23 +0200

    Created Users table

---
+++ DB.DepotEice/dbo/Tables/Users.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] NVARCHAR(MAX) NOT NULL PRIMARY KEY, 
+    [Email] NVARCHAR(255) NOT NULL, 
+    [NormalizedEmail] NVARCHAR(255) NOT NULL,
+    [PasswordHash] BINARY(64) NOT NULL, 
+    [Salt] UNIQUEIDENTIFIER NOT NULL,
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [Lastname] NVARCHAR(100) NOT NULL,
+    [ProfilePicture] NVARCHAR(255) NOT NULL, 
+    [BirthDate] DATE NOT NULL, 
+    [ConcurrencyStamp] UNIQUEIDENTIFIER NOT NULL, 
+    [SecurityStamp] UNIQUEIDENTIFIER NOT NULL, 
+    [CreatedAt] DATETIME NOT NULL, 
+    [UpdatedAt] DATETIME NULL, 
+    [DeletedAt] DATETIME NULL
+)
