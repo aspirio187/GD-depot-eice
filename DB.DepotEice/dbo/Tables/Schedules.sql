5f98be6798cbbffa15ecc505456e9b2b3b59dd15
Soultan Hatsijev <soultan_98@hotmail.com>
2022-06-26 20:27:13 +0200

    created Schedules table

---
+++ DB.DepotEice/dbo/Tables/Schedules.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Schedules]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Title] NVARCHAR(100) NULL,
+	[Details] NVARCHAR(MAX) NULL,
+	[StartTime] DATETIME NOT NULL,
+	[EndTime] DATETIME NOT NULL,
+	[ModuleId] INT NOT NULL,
+	CONSTRAINT [FK_Schedules_Module] FOREIGN KEY ([ModuleId]) REFERENCES [Modules]([Id])
+)
