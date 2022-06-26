87f8dfe91a8ba3e50ccbbcd844923466ad04b8cf
Soultan Hatsijev <soultan_98@hotmail.com>
2022-06-26 20:34:37 +0200

    Created opening hours and appoitments tables

---
+++ DB.DepotEice/dbo/Tables/Appointments.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Appointments]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[StartAt] DATETIME NOT NULL,
+	[EndAt] DATETIME NOT NULL,
+	[Accepted] BIT NOT NULL,
+	[UserId] NVARCHAR(36) NOT NULL,
+	CONSTRAINT [FK_Appointments_User] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id])
+)
