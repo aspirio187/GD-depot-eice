500005bfa135f13f4af5a1923f68bd9653f8e61a
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-05 21:58:14 +0200

    created delete and update appointment stored procedure

---
+++ DB.DepotEice/dbo/StoredProcedures/spUpdateAppointment.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUpdateAppointment]
+	@id INT,
+	@startAt DATETIME,
+	@endAt DATETIME,
+	@accepted BIT
+AS
+	UPDATE [Appointments]
+	SET [StartAt] = @startAt, [EndAt] = @endAt, [Accepted] = @accepted
+	WHERE [Appointments].[Id] = @id
+GO;
