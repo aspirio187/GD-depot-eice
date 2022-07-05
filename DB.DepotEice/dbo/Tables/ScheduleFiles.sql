3787ddc0da38564eca3e8b6bf972e28f1aa7b44a
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-05 22:48:34 +0200

    added  IDENTITY(1,1) (auto increment)

--- DB.DepotEice/dbo/Tables/ScheduleFiles.sql
+++ DB.DepotEice/dbo/Tables/ScheduleFiles.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ScheduleFiles]
 (
-	[Id] INT NOT NULL PRIMARY KEY,
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	[FilePath] NVARCHAR(255) NOT NULL,
 	[ScheduleId] INT NOT NULL,
 	CONSTRAINT [FK_ScheduleFiles_Schedule] FOREIGN KEY ([ScheduleId]) REFERENCES [Schedules]([Id])
