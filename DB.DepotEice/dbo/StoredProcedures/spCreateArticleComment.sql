ea7b6c686046e5cf4c48665d6cca98e9d7ad2726
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-11 21:50:58 +0200

    updated articleComment table and create spCreateArticleComment

---
+++ DB.DepotEice/dbo/StoredProcedures/spCreateArticleComment.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[spCreateArticleComment]
+	@note INT,
+	@review NVARCHAR(500),
+	@articleId INT,
+	@userId UNIQUEIDENTIFIER
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	INSERT INTO [dbo].[ArticleComments]
+	(
+		Note,
+		Review,
+		ArticleId,
+		UserId
+	)
+	VALUES
+	(
+		@note,
+		@review,
+		@articleId,
+		@userId
+	);
+END
