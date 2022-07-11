d8fe434750d2dc5edf61e0c3f71eadc994486a40
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-11 21:57:28 +0200

    update message table and create sp create message

---
+++ DB.DepotEice/dbo/StoredProcedures/spCreateMessage.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[spCreateMessage]
+	@body NVARCHAR(1000),
+	@userFromId UNIQUEIDENTIFIER,
+	@userToId UNIQUEIDENTIFIER
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO [dbo].[Messages]
+	(
+		Body,
+		UserFromId,
+		UserToId
+	)
+	VALUES
+	(
+		@body,
+		@userFromId,
+		@userToId
+	);
+
+	SELECT SCOPE_IDENTITY();
+END
