6d1f05ffeb11f10dc63e36a042e64758d93897b4
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-11 21:47:21 +0200

    refactored spCreate module

--- DB.DepotEice/dbo/StoredProcedures/spCreateModule.sql
+++ DB.DepotEice/dbo/StoredProcedures/spCreateModule.sql
@@ -2,8 +2,19 @@
 	@name NVARCHAR(255),
 	@description NVARCHAR(1000)
 AS
-	INSERT INTO [Modules]([Name], [Description])
-	VALUES (@name, @description);
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO [dbo].[Modules]
+	(
+		[Name],
+		[Description]
+	)
+	VALUES
+	(
+		@name,
+		@description
+	);
 
 	SELECT SCOPE_IDENTITY();
-GO;
+END
