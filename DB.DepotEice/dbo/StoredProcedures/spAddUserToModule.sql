bdd7a29487f4d1ed5e1bb96c81ddcafaecd5c7ad
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-11 22:13:05 +0200

    create sp add user to module

---
+++ DB.DepotEice/dbo/StoredProcedures/spAddUserToModule.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[spAddUserToModule]
+	@userId UNIQUEIDENTIFIER,
+	@moduleId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO [dbo].[UsersModules]
+	(
+		UserId,
+		ModuleId
+	)
+	VALUES
+	(
+		@userId,
+		@moduleId
+	);
+END
