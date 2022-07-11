a3c15d8a31dfa4ca40a6a222781141fa60b221e7
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-11 21:59:45 +0200

    update openinghours datetime tables and create openinghour sp

---
+++ DB.DepotEice/dbo/StoredProcedures/spCreateOpeningHour.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[spCreateOpeningHour]
+	@openAt DATETIME2,
+	@closeAt DATETIME2
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO [dbo].[OpeningHours]
+	(
+		OpenAt,
+		CloseAt
+	)
+	VALUES
+	(
+		@openAt,
+		@closeAt
+	);
+
+	SELECT SCOPE_IDENTITY();
+END
