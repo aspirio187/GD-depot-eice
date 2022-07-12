1d068208f7e7c90ebc40c081c1b209373d9accc4
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-12 16:33:12 +0200

    Modified UsersTokens tables types and default value. Created sp to create a userToken field

---
+++ DB.DepotEice/dbo/StoredProcedures/spCreateUserToken.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[spCreateUserToken]
+	@type NVARCHAR(100),
+	@value NVARCHAR(MAX),
+	@expirationDate DATETIME2,
+	@userId UNIQUEIDENTIFIER
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO [dbo].[UsersTokens]
+	(
+		[Type],
+		[Value],
+		[ExpirationDate],
+		[UserId]
+	)
+	OUTPUT inserted.Id
+	VALUES
+	(
+		@type,
+		@value,
+		@expirationDate,
+		@userId
+	);
+
+END
+
