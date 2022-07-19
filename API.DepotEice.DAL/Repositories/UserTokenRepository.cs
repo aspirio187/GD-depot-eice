8dbc446676a482ce8bfbfa1f6b8aa18f17cb0af3
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-19 23:41:06 +0200

    Implemented UserToken repository

--- API.DepotEice.DAL/Repositories/UserTokenRepository.cs
+++ API.DepotEice.DAL/Repositories/UserTokenRepository.cs
@@ -1,6 +1,7 @@
 ﻿using API.DepotEice.DAL.Entities;
 using API.DepotEice.DAL.IRepositories;
 using AutoMapper;
+using DepotEice.Shared.Exceptions;
 using DevHopTools.Connection;
 using System;
 using System.Collections.Generic;
@@ -31,6 +32,9 @@ namespace API.DepotEice.DAL.Repositories
             _connection = connection;
         }
 
+        /// <inheritdoc/>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="DatabaseScalarNullException"></exception>
         public string Create(UserTokenEntity entity)
         {
             if (entity is null)
@@ -43,29 +47,83 @@ namespace API.DepotEice.DAL.Repositories
             command.AddParameter("type", entity.Type);
             command.AddParameter("expirationDate", entity.ExpirationDateTime);
             command.AddParameter("userId", entity.UserId);
-            command.AddParameter("userSecurityStamp", entity.)
+            command.AddParameter("userSecurityStamp", entity.UserSecurityStamp);
+
+            string? scalarResult = _connection.ExecuteScalar(command).ToString();
+
+            if (string.IsNullOrEmpty(scalarResult))
+            {
+                throw new DatabaseScalarNullException(nameof(scalarResult));
+            }
+
+            return scalarResult;
         }
 
+        /// <inheritdoc/>
+        /// <exception cref="ArgumentNullException"></exception>
         public bool Delete(UserTokenEntity entity)
         {
-            throw new NotImplementedException();
+            if (entity is null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            Command command = new Command("spDeleteUserToken", true);
+
+            command.AddParameter("id", entity.Id);
+
+            return _connection.ExecuteNonQuery(command) > 0;
         }
 
+        /// <summary>
+        /// Not implemented
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public IEnumerable<UserTokenEntity> GetAll()
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
+        /// <exception cref="ArgumentNullException"></exception>
         public UserTokenEntity? GetByKey(string key)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            Command command = new Command("spGetUserToken", true);
+
+            command.AddParameter("id", key);
+
+            return _connection
+                .ExecuteReader(command, userToken => _mapper.Map<UserTokenEntity>(userToken))
+                .SingleOrDefault();
         }
 
         public IEnumerable<UserTokenEntity> GetUserTokens(string userId)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(userId))
+            {
+                throw new ArgumentNullException(nameof(userId));
+            }
+
+            Command command = new Command("spGetUserTokens", true);
+
+            command.AddParameter("userId", userId);
+
+            return _connection
+                .ExecuteReader(command, userToken => _mapper.Map<UserTokenEntity>(userToken));
         }
 
+        /// <summary>
+        /// Not implemented
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public bool Update(UserTokenEntity entity)
         {
             throw new NotImplementedException();
